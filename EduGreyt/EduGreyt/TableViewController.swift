944a24b02f3cb0e4c00ffcf3207edada2eb800bd
AllonsyThor <kt.kllhr@gmail.com>
2016-01-21 17:36:40 -0800

    added Parse to project

---
+++ EduGreyt/EduGreyt/TableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TableViewController.swift
+//  EduGreyt
+//
+//  Created by Catherine Kelleher on 11/25/15.
+//  Copyright © 2015 Katie Kelleher. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+  
+    var topics: [String] = ["Apperance", "Temperament", "As Pets", "Health", "Interesting Facts" ]
+  
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return topics.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = table
+        
+    }
+    
+    
+}
---
+++ EduGreyt/Pods/Target Support Files/Pods-EduGreyt/Pods-EduGreyt-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EduGreyt : NSObject
+@end
+@implementation PodsDummy_Pods_EduGreyt
+@end
