f8e2ac4b7f333b66199f40c5d0c53a7bf3388dc0
AllonsyThor <kt.kllhr@gmail.com>
2016-02-06 19:14:43 -0800

    added permission request to access users calendar

--- EduGreyt/EduGreyt/EventsViewController.swift
+++ EduGreyt/EduGreyt/EventsViewController.swift
@@ -10,18 +10,91 @@ import UIKit
 import EventKit
 import MapKit
 
-class EventsViewController: UIViewController {
+class EventsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    //Creating EventStore
     let eventStore = EKEventStore()
     
-   
-
+    @IBOutlet weak var needPermissionView: UIView!
+    @IBOutlet weak var calendarsTableView: UITableView!
+    
+    var calendars: [EKCalendar]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
+        
     }
     
-
-}
+    override func viewWillAppear(animated: Bool) {
+        checkCalendarAuthorizationStatus()
+    }
+    
+    func checkCalendarAuthorizationStatus() {
+        let status = EKEventStore.authorizationStatusForEntityType(EKEntityType.Event)
+        
+        switch (status) {
+        case EKAuthorizationStatus.NotDetermined:
+            // This happens on first-run
+            requestAccessToCalendar()
+        case EKAuthorizationStatus.Authorized:
+            // Things are in line with being able to show the calendars in the table view
+            loadCalendars()
+            refreshTableView()
+        case EKAuthorizationStatus.Restricted, EKAuthorizationStatus.Denied:
+            // We need to help them give us permission
+            needPermissionView.fadeIn()
+        }
+    }
+    
+    func requestAccessToCalendar() {
+        eventStore.requestAccessToEntityType(EKEntityType.Event, completion: {
+            (accessGranted: Bool, error: NSError?) in
+            
+            if accessGranted == true {
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.loadCalendars()
+                    self.refreshTableView()
+                })
+            } else {
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.needPermissionView.fadeIn()
+                })
+            }
+        })
+    }
+    
+    func loadCalendars() {
+        self.calendars = eventStore.calendarsForEntityType(EKEntityType.Event)
+    }
+    
+    func refreshTableView() {
+        calendarsTableView.hidden = false
+        calendarsTableView.reloadData()
+    }
+    
+    @IBAction func goToSettingsButtonTapped(sender: UIButton) {
+        let openSettingsUrl = NSURL(string: UIApplicationOpenSettingsURLString)
+        UIApplication.sharedApplication().openURL(openSettingsUrl!)
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let calendars = self.calendars {
+            return calendars.count
+        }
+        
+        return 0
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("basicCell")!
+        
+        if let calendars = self.calendars {
+            let calendarName = calendars[indexPath.row].title
+            cell.textLabel?.text = calendarName
+        } else {
+            cell.textLabel?.text = "Unknown Calendar Name"
+        }
+        
+        return cell
+    }}
---
+++ EduGreyt/EduGreyt/UIViewExtensions.swift
@@ -0,0 +1,23 @@
+//
+//  UIViewExtensions.swift
+//  EduGreyt
+//
+//  Created by Catherine Kelleher on 2/6/16.
+//  Copyright © 2016 Katie Kelleher. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func fadeIn(duration: NSTimeInterval = 1.0, delay: NSTimeInterval = 0.0, completion: ((Bool) -> Void) = {(finished: Bool) -> Void in}) {
+        UIView.animateWithDuration(duration, delay: delay, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+            self.alpha = 1.0
+            }, completion: completion)  }
+    
+    func fadeOut(duration: NSTimeInterval = 1.0, delay: NSTimeInterval = 0.0, completion: (Bool) -> Void = {(finished: Bool) -> Void in}) {
+        UIView.animateWithDuration(duration, delay: delay, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+            self.alpha = 0.0
+            }, completion: completion)
+    }
+}
